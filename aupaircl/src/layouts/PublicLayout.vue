d0770793b6a91e9eab4378d41e19ecfe18767cc6
ChristianAlonso16 <105463895+ChristianAlonso16@users.noreply.github.com>
2024-05-22 13:54:19 -0600

    [Add] Feature fuction

--- aupaircl/src/layouts/PublicLayout.vue
+++ aupaircl/src/layouts/PublicLayout.vue
@@ -1,44 +1,41 @@
 <template>
-    <div>
-      <b-container fluid>
-        <b-row class="row">
-          <Header />
-        </b-row>
-        <div class="main-content d-flex flex-column text-center">
-          <b-col class="col">
-            <div class="">
-              <router-view />
-            </div>
-          </b-col>
-          <b-col class="col-12">
-            <b-container fluid>
-              <Footer/>
-            </b-container>
-          </b-col>
-        </div>
-      </b-container>
+  <b-container fluid>
+    <b-container>
+      <Header />
+    </b-container>
+    <div class="main-content d-flex flex-column text-center">
+      <b-col class="col">
+        <router-view />
+      </b-col>
+      <b-col class="col-12">
+        <b-container fluid>
+          <Footer />
+        </b-container>
+      </b-col>
     </div>
-  </template>
-  
-  <script>
-  import Footer from '../components/Footer/Footer.vue';
-  import Header from '../components/Header/Header.vue';
-  export default {
-    data() {
-      return {
-      }
-    }, components: {
-      Footer, Header
+  </b-container>
+</template>
+
+<script>
+import Footer from '../components/Footer/Footer.vue';
+import Header from '../components/Header/Header.vue';
+export default {
+  data() {
+    return {
     }
+  }, components: {
+    Footer, Header
   }
-  </script>
-  <style scoped>
+}
+</script>
+<style scoped>
+.main-content {
+  margin-top: 65px;
+}
+
+@media (max-width: 30em) {
   .main-content {
-    margin-top: 65px;
+    margin-top: 67px;
   }
-  @media (max-width: 30em) {
-    .main-content{
-      margin-top: 67px;
-    }
-  }
-  </style>
+}
+</style>
---
+++ aupaircl/src/views/FeatureFuction/FeatureFuction.vue
@@ -0,0 +1,75 @@
+<template lang="">
+    <div class="mt-3">
+        <section class="features-section-g"> 
+            <strong  style="font-size: 36px;">¿Cómo funciona?</strong> 
+            <b-row class="py-5 my-5 ms-5">
+            <b-col col lg="2" class="mt-3">
+                <div>
+                    <p class="numbers"><strong>1</strong></p>
+                    <br>
+                    <div class="morad">
+                        <strong>Crear perfil</strong>  
+                    </div>
+                     </div>
+            </b-col>
+            <b-col col lg="2" class="mt-3">
+                <div>
+                    <p class="numbers"><strong>2</strong></p>
+                    <br>
+                    <div class="morad">
+                        <strong>Buscar</strong>  
+                    </div>
+                     </div>
+            </b-col>
+            <b-col col lg="2" class="mt-3">
+                <div>
+                    <p class="numbers"><strong>3</strong></p>
+                    <br>
+                    <div class="morad">
+                        <strong>Estar en contacto</strong>  
+                    </div>
+                     </div>
+            </b-col>
+            <b-col col lg="2" class="mt-3">
+                <div>
+                    <p class="numbers"><strong>4</strong></p>
+                    <br>
+                    <div class="morad">
+                        <strong>Mensajes de intercambio</strong>  
+                    </div>
+                     </div>
+            </b-col>
+            <b-col col lg="2" class="mt-3">
+                <div>
+                    <p class="numbers"><strong>5</strong></p>
+                    <br>
+                    <div class="morad">
+                        <strong>Firmar un contrato</strong>  
+                    </div>
+                     </div>
+            </b-col>
+        </b-row>
+    </section>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style scoped>
+.numbers {
+    height: 35px;
+    width: 35px;
+    color: #fff;
+    font-size: 25px;
+    background-color: #EF0082;
+    border-radius: 50%;
+    box-shadow: 1px 2px 1px 1px #5D5854;
+    display: inline-block;
+}
+
+.morad {
+    color: #980079 !important;
+}
+</style>
