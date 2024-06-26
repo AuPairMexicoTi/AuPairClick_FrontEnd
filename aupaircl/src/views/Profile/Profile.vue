65bf222e83a2b6f0a57c8cfc8c1e02be2d76bcc2
ChristianAlonso16 <105463895+ChristianAlonso16@users.noreply.github.com>
2024-06-26 17:58:40 -0600

    [Add] profile

---
+++ aupaircl/src/views/Profile/Profile.vue
@@ -0,0 +1,37 @@
+<template lang="">
+   <b-container>
+    <b-row class="justify-content-center">
+        <b-col cols="12" md="8">
+            <b-card class="p-3">
+                <b-row class="justify-content-center">
+                    <b-col cols="12" md="2" class="text-md-center text-center">
+                        <b-avatar variant="info" src="https://placekitten.com/300/300"></b-avatar>
+                    </b-col>
+                    <b-col cols="12" md="10" class="text-start">
+                        <h3>Brandon Hernandez</h3>
+                        <p>Hola, Bienvenid@ a tu perfil</p>
+                    </b-col>
+                </b-row>
+                <div class="border-top"></div>
+                <b-row class="mt-4">
+                    <b-col cols="12">
+                        <h4>Nombre completo</h4>
+                        <p>Por favor de llenar el nombre y el apellido</p>
+                    </b-col>
+                    <b-col cols="12" md="5">
+                        <p></p>
+                    </b-col>
+                </b-row>
+            </b-card>
+        </b-col>
+    </b-row>
+   </b-container>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
