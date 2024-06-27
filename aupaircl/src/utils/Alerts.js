cb1b4e640c998d551300c36f74df1e7bcd50cc1c
ChristianAlonso16 <105463895+ChristianAlonso16@users.noreply.github.com>
2024-06-27 11:46:34 -0600

    [Add] Utils alerts

---
+++ aupaircl/src/utils/Alerts.js
@@ -0,0 +1,32 @@
+import Swal from "sweetalert2";
+const showMessageSuccess= async(message, status)=>{
+    const Toast = Swal.mixin({
+      toast: true,
+      position: "top-end",
+      showConfirmButton: false,
+      timer: 2000,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.onmouseenter = Swal.stopTimer;
+        toast.onmouseleave = Swal.resumeTimer;
+      }
+    });
+    Toast.fire({
+      icon: status,
+      title: message
+    });
+}
+const loading=async()=>{
+  		Swal.fire({
+			title: "Cargando...",
+			text: 'Por favor espere...',
+			allowOutsideClick: false,
+			showConfirmButton: false,
+			willOpen: () => {
+				Swal.showLoading();
+			},
+		});
+}
+export default{
+    showMessageSuccess,loading
+}
